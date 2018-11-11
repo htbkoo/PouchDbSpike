8af0ffff6c0bfcd265e2ba8ef824e4343f4c5c89
htbkoo <h1017760@connect.hku.hk>
2018-11-11 16:53:29 +0800

    added more tests to spike usage of pouchDB htbkoo/PouchDbSpike

--- pouchSpike.test.ts
+++ pouchSpike.test.ts
@@ -4,12 +4,29 @@ import {addItem} from "./pouchSpike";
 import * as pouchdb_adapter_memory from "pouchdb-adapter-memory";
 
 PouchDB.plugin(pouchdb_adapter_memory);
-// PouchDB.plugin(require('pouchdb-adapter-memory'));
+const db = new PouchDB('in_memory_database', {adapter: 'memory'});
 
 describe('pouchDB', function () {
     it('should add to local database', function () {
-        const db = new PouchDB('in_memory_database', {adapter: 'memory'});
         return addItem(db, "")
             .then(data => expect(data).toBeDefined());
+    });
+
+    it('should update document correctly in local database', function () {
+        return addItem(db, "some")
+            .then(result => db.get(result.id))
+            .then(doc => {
+                expect(doc).toBeDefined();
+                return doc;
+            })
+            .then(doc => db.put({
+                ...doc,
+                title: "another"
+            }))
+            .then(result => {
+                expect(result.ok).toEqual(true);
+                return db.get(result.id);
+            })
+            .then(doc=>expect(doc.title).toEqual("another"));
     });
 });
